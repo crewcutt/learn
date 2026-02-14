80d06a73ff79ba54eb5b483dcdebfd7c5bf93a76
crewcutt <crewcutt@gmail.com>
2026-02-14 13:11:56 +0300

    начало

--- capter04/ex01.py
+++ capter04/ex01.py
@@ -1,13 +1,18 @@
+import math
 import turtle
+bob = turtle.Turtle()
+turtle.pendown()
 
-import turtle
+def polyline(t: turtle.Turtle, n: int, length: float, angle: float):
+   """Рисует n отрезков с заданной длиной length и углами angle
+   (в градусах) между ними черепашкой t.
+   """
+   for i in range(n):
+       t.forward(length)
+       t.left(angle)
 
-def polygon(t: turtle.Turtle, n: int, length: float):
-   angle = 360 / n
-   polyline(t, n, length, angle)
-
-
-
+def circle(t: turtle.Turtle, radius: float):
+    arc(t, radius, 360)
 
 def arc(t: turtle.Turtle, radius: float, angle: float):
    arc_length = 2 * math.pi * radius * angle / 360
@@ -16,3 +21,6 @@ def arc(t: turtle.Turtle, radius: float, angle: float):
    step_angle = angle / n
    polyline(t, n, step_length, step_angle)
 
+#polygon(bob, n=7, length=70)
+circle(bob, 100)
+turtle.mainloop()
